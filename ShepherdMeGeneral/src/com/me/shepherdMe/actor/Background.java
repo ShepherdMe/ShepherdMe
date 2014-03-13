7da4b1cd808ace6310ac2018fe2846d27638c9da
xinin <diprieto.1992@gmail.com>
2014-03-13 16:43:07 +0100

    Unido Dog y Sheep sin conflictos

--- ShepherdMeGeneral/src/com/me/shepherdMe/actor/Background.java
+++ ShepherdMeGeneral/src/com/me/shepherdMe/actor/Background.java
@@ -41,17 +41,7 @@ public class Background extends Actor {
 				return true;
 		    }
 		});	*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	
-			
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
+
 	}
 	
 	public void draw(SpriteBatch batch, float parentApha)
--- ShepherdMeGeneral/src/com/me/shepherdMe/table/LogicaLevel.java
+++ ShepherdMeGeneral/src/com/me/shepherdMe/table/LogicaLevel.java
@@ -1,152 +1,65 @@
 package com.me.shepherdMe.table;
 
-
-import aurelienribon.tweenengine.equations.Back;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.me.shepherdMe.ShepherdMe;
 import com.me.shepherdMe.actor.Background;
 import com.me.shepherdMe.actor.Dog;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import com.me.shepherdMe.actor.Sheep;
-=======
 import com.me.shepherdMe.actor.input.BackgroundUserInput;
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
-import com.me.shepherdMe.actor.input.BackgroundUserInput;
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
-import com.me.shepherdMe.actor.input.BackgroundUserInput;
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
 import com.me.shepherdMe.screens.Level;
 
-public class LogicaLevel extends Table{
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	ShepherdMe game;
-	Background background;
-	Dog dog;
-	Sheep sheep1,sheep2;
-=======
+public class LogicaLevel extends Table {
+
+	private Sheep sheep1, sheep2;
+
 	private ShepherdMe game;
 	private Background background;
 	private Dog dog;
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
-	private ShepherdMe game;
-	private Background background;
-	private Dog dog;
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
-	private ShepherdMe game;
-	private Background background;
-	private Dog dog;
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-	
+
 	public LogicaLevel(ShepherdMe game, Level screen) {
 		setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		setClip(true);
-		this.game=game;
-		this.background= new Background(game,this);
+		this.game = game;
+		this.background = new Background(game, this);
 		this.background.addListener(new BackgroundUserInput(this.background));
 		addActor(background);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		
-		//this.dog= new Dog(game);
-		//addActor(dog);
-		
-		this.sheep1=new Sheep(game,250,100);
-		this.sheep2=new Sheep(game,50,400);
+		this.dog = new Dog(game);
+		addActor(dog);
+
+		this.sheep1 = new Sheep(game, 250, 100);
+		this.sheep2 = new Sheep(game, 50, 400);
 		actionSheep();
-		
+
 		addActor(sheep1);
 		addActor(sheep2);
-=======
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-		this.dog= new Dog(game);
-		//this.dog.addListener(new DogUserInput(this.dog));
-		addActor(dog);
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-		
+
 	}
-	
+
 	@Override
-	public void act(float delta)
-	{
+	public void act(float delta) {
 		super.act(delta);
 	}
-	
+
 	@Override
-	public void draw(SpriteBatch batch, float parentAlpha)
-	{
+	public void draw(SpriteBatch batch, float parentAlpha) {
 		batch.setColor(Color.WHITE);
 		super.draw(batch, parentAlpha);
 	}
-	
-	public Dog getDog()
-<<<<<<< HEAD
-<<<<<<< HEAD
-	{
-		return this.dog;
-	}
-	
-	public Background GetBackground()
-	{
-		return this.background;
-	}
-<<<<<<< HEAD
-	public void actionSheep()
-	{
-		int with=480;
-		int height=800;
-		
-	}
-	
 
-=======
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
-	{
+	public Dog getDog() {
 		return this.dog;
 	}
-	
-	public Background GetBackground()
-	{
+
+	public Background GetBackground() {
 		return this.background;
 	}
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
-=======
-	{
-		return this.dog;
+
+	public void actionSheep() {
+		int with = 480;
+		int height = 800;
+
 	}
-	
-	public Background GetBackground()
-	{
-		return this.background;
-	}
->>>>>>> a271b89b6f9eaec3ac3861c2ad681838ebb78ea9
 }
