7e889562a986b7b5900cf67f3167fd8fa75918d8
maxufly <jesusnieto11@hotmail.com>
2014-04-15 16:52:38 +0200

    Botón de atrás funciona en la pantalla de elegir nivel

--- ShepherdMeGeneral/src/com/me/shepherdMe/screens/LevelChooser.java
+++ ShepherdMeGeneral/src/com/me/shepherdMe/screens/LevelChooser.java
@@ -2,12 +2,14 @@ package com.me.shepherdMe.screens;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimerTask;
 
 import utils.GraphicManager;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -37,7 +39,7 @@ public class LevelChooser implements Screen {
 	private ShepherdMe game;
 	private Image backArrow;
 	private List<Image> imagenesNiveles1, imagenesNiveles2;
-	private Image arrowRight, arrowLeft, dog1,dog2;
+	private Image arrowRight, arrowLeft, dog1, dog2;
 	private MainMenu mm;
 
 	public LevelChooser(ShepherdMe game, MainMenu mm) {
@@ -52,6 +54,11 @@ public class LevelChooser implements Screen {
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Keys.BACK)) {
+			((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu(
+					game));
+		}
 
 		batchBackground.begin();
 		batchBackground.disableBlending();
@@ -74,16 +81,20 @@ public class LevelChooser implements Screen {
 	public void show() {
 		// TODO Auto-generated method stub
 		batchBackground = new SpriteBatch();
-		textureBackground = new Texture(Gdx.files.internal("img/chooseLevel/fondo.png"));
+		textureBackground = new Texture(
+				Gdx.files.internal("img/chooseLevel/fondo.png"));
 		backgroundSprite = new Sprite(textureBackground);
-		backgroundSprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		backgroundSprite.setSize(Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
 
 		stage = new Stage();
 
 		Gdx.input.setInputProcessor(stage);
 
-		Image imageGreen = new Image(new Texture(Gdx.files.internal("img/green.png")));
-		Image imageBlue = new Image(new Texture(Gdx.files.internal("img/green.png")));
+		Image imageGreen = new Image(new Texture(
+				Gdx.files.internal("img/green.png")));
+		Image imageBlue = new Image(new Texture(
+				Gdx.files.internal("img/green.png")));
 		float imageHeight = Gdx.graphics.getHeight() / 3;
 		float imageWidth = Gdx.graphics.getWidth() / 4;
 		imageGreen.setSize(imageWidth, imageHeight);
@@ -91,20 +102,37 @@ public class LevelChooser implements Screen {
 
 		// Choose level text
 		Image chooseText = new Image(new Texture(
-		Gdx.files.internal("img/chooseLevel/chooseLevel.png")));
-		chooseText.setSize(Gdx.graphics.getWidth()/1.5f,Gdx.graphics.getHeight()/4.2f);
-		chooseText.setPosition(Gdx.graphics.getWidth()/2-chooseText.getWidth()/2,Gdx.graphics.getHeight()-chooseText.getHeight()/0.9f);
+				Gdx.files.internal("img/chooseLevel/chooseLevel.png")));
+		chooseText.setSize(Gdx.graphics.getWidth() / 1.5f,
+				Gdx.graphics.getHeight() / 4.2f);
+		chooseText.setPosition(
+				Gdx.graphics.getWidth() / 2 - chooseText.getWidth() / 2,
+				Gdx.graphics.getHeight() - chooseText.getHeight() / 0.9f);
 
-		dog1 = new Image(new Texture(Gdx.files.internal("img/chooseLevel/dog1.png")));
-		dog1.setSize(Gdx.graphics.getWidth()/4.5f, Gdx.graphics.getWidth()/4.5f);
-		
-		
-		dog2 = new Image(new Texture(Gdx.files.internal("img/chooseLevel/dog2.png")));
-		dog2.setSize(Gdx.graphics.getWidth()/4.5f, Gdx.graphics.getWidth()/4.5f);
-		dog2.setPosition(Gdx.graphics.getWidth(),-dog2.getHeight()*2);//Desplazada dado que este al principio esta mas abajo para la animacion
-		
+		dog1 = new Image(new Texture(
+				Gdx.files.internal("img/chooseLevel/dog1.png")));
+		dog1.setSize(Gdx.graphics.getWidth() / 4.5f,
+				Gdx.graphics.getWidth() / 4.5f);
+
+		dog2 = new Image(new Texture(
+				Gdx.files.internal("img/chooseLevel/dog2.png")));
+		dog2.setSize(Gdx.graphics.getWidth() / 4.5f,
+				Gdx.graphics.getWidth() / 4.5f);
+		dog2.setPosition(Gdx.graphics.getWidth(), -dog2.getHeight() * 2);// Desplazada
+																			// dado
+																			// que
+																			// este
+																			// al
+																			// principio
+																			// esta
+																			// mas
+																			// abajo
+																			// para
+																			// la
+																			// animacion
+
 		arrowRight = new Image(new Texture(
-		Gdx.files.internal("img/chooseLevel/arrowRight.png")));
+				Gdx.files.internal("img/chooseLevel/arrowRight.png")));
 		arrowRight.setX(Gdx.graphics.getWidth() * 7 / 8);
 		arrowRight.setY(Gdx.graphics.getHeight() * 1 / 7);
 		arrowRight.setHeight(imageHeight * 2 + 5);
@@ -137,7 +165,7 @@ public class LevelChooser implements Screen {
 					MoveByAction mba = new MoveByAction();
 					mba.setAmountX(-Gdx.graphics.getWidth());
 					mba.setDuration(0.75f);
-					//imagen.addAction(mba);
+					// imagen.addAction(mba);
 
 					AlphaAction apA = new AlphaAction();
 					apA.setAlpha(1);
@@ -146,28 +174,28 @@ public class LevelChooser implements Screen {
 
 					ParallelAction pa = new ParallelAction(mba, apA);
 					imagen.addAction(pa);
-					
 
 				}
 				arrowLeft.setVisible(true);
 				arrowRight.setVisible(false);
-				//event.getRelatedActor().setVisible(false);
+				// event.getRelatedActor().setVisible(false);
 
 				MoveByAction mba = new MoveByAction();
-				mba.setAmount(-dog1.getWidth(), -dog1.getHeight()*2);
+				mba.setAmount(-dog1.getWidth(), -dog1.getHeight() * 2);
 				mba.setDuration(1f);
 				dog1.addAction(mba);
-				
+
 				MoveByAction mba2 = new MoveByAction();
-				mba2.setAmount(-dog2.getWidth(), dog2.getHeight()*2);
+				mba2.setAmount(-dog2.getWidth(), dog2.getHeight() * 2);
 				mba2.setDuration(1f);
 				dog2.addAction(mba2);
-				
+
 				return true;
 			}
 		});
 
-		arrowLeft = new Image(new Texture(Gdx.files.internal("img/chooseLevel/arrowLeft.png")));
+		arrowLeft = new Image(new Texture(
+				Gdx.files.internal("img/chooseLevel/arrowLeft.png")));
 		arrowLeft.setX(Gdx.graphics.getWidth() / 25);
 		arrowLeft.setY(Gdx.graphics.getHeight() * 1 / 7);
 		arrowLeft.setHeight(imageHeight * 2 + 5);
@@ -194,13 +222,13 @@ public class LevelChooser implements Screen {
 					imagen.addAction(pa);
 
 				}
-				
+
 				for (Image imagen : imagenesNiveles1) {
 
 					MoveByAction mba = new MoveByAction();
 					mba.setAmountX(Gdx.graphics.getWidth());
 					mba.setDuration(0.75f);
-					//imagen.addAction(mba);
+					// imagen.addAction(mba);
 
 					AlphaAction apA = new AlphaAction();
 					apA.setAlpha(1);
@@ -209,40 +237,38 @@ public class LevelChooser implements Screen {
 
 					ParallelAction pa = new ParallelAction(mba, apA);
 					imagen.addAction(pa);
-					
 
 				}
-								
+
 				arrowLeft.setVisible(false);
 				arrowRight.setVisible(true);
-				
+
 				MoveByAction mba = new MoveByAction();
-				mba.setAmount(dog1.getWidth(), dog1.getHeight()*2);
+				mba.setAmount(dog1.getWidth(), dog1.getHeight() * 2);
 				mba.setDuration(1f);
 				dog1.addAction(mba);
-				
+
 				MoveByAction mba2 = new MoveByAction();
-				mba2.setAmount(dog2.getWidth(), -dog2.getHeight()*2);
+				mba2.setAmount(dog2.getWidth(), -dog2.getHeight() * 2);
 				mba2.setDuration(1f);
 				dog2.addAction(mba2);
-				
-				
+
 				return true;
 			}
 		});
 
 		this.crearNiveles();
 
-		
-		
-		backArrow = new Image(new Texture(Gdx.files.internal("img/chooseLevel/back.png")));
+		backArrow = new Image(new Texture(
+				Gdx.files.internal("img/chooseLevel/back.png")));
 		int width = Gdx.graphics.getWidth(), height = Gdx.graphics.getHeight();
-		backArrow.setBounds(width / 20, 7 * height / 8, width / 11, height / 11);
-		
+		backArrow
+				.setBounds(width / 20, 7 * height / 8, width / 11, height / 11);
+
 		backArrow.addListener(new InputListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {				
+					int pointer, int button) {
 				((Game) Gdx.app.getApplicationListener())
 						.setScreen(new MainMenu(game));
 				return true;
@@ -269,7 +295,8 @@ public class LevelChooser implements Screen {
 				.getHeight() * 2 / 5, deltaX = 0;
 		final LevelChooser levelChooser = this;
 		for (int i = 0; i < 6; i++) {
-			image = new Image(new Texture(Gdx.files.internal("img/chooseLevel/cartel.png")));
+			image = new Image(new Texture(
+					Gdx.files.internal("img/chooseLevel/cartel.png")));
 			this.imagenesNiveles1.add(image);
 			image.setBounds(initialX + imageWidth * deltaX + 2, initialY,
 					imageWidth, imageHeight);
@@ -290,15 +317,16 @@ public class LevelChooser implements Screen {
 				deltaX = 0;
 			}
 		}
-		
+
 		initialX = Gdx.graphics.getWidth() * 3 / 16;
 		initialY = Gdx.graphics.getHeight() * 2 / 5;
 		deltaX = 0;
 		for (int i = 0; i < 6; i++) {
-			image = new Image(new Texture(Gdx.files.internal("img/chooseLevel/cartel.png")));
+			image = new Image(new Texture(
+					Gdx.files.internal("img/chooseLevel/cartel.png")));
 			this.imagenesNiveles2.add(image);
-			image.setBounds(Gdx.graphics.getWidth()+initialX + imageWidth * deltaX + 2, initialY,
-					imageWidth, imageHeight);
+			image.setBounds(Gdx.graphics.getWidth() + initialX + imageWidth
+					* deltaX + 2, initialY, imageWidth, imageHeight);
 			deltaX++;
 			if (i == 0) {
 				image.addListener(new InputListener() {
@@ -306,7 +334,7 @@ public class LevelChooser implements Screen {
 					public boolean touchDown(InputEvent event, float x,
 							float y, int pointer, int button) {
 						((Game) Gdx.app.getApplicationListener())
-								.setScreen(new Level(game,levelChooser));
+								.setScreen(new Level(game, levelChooser));
 						return true;
 					}
 				});
